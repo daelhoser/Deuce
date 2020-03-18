3b04e62567df7b1fe547a26b7c2397de26f3ef63
Jose <daelhoser@gmail.com>
2020-03-17 17:38:02 -0700

    Include API/ directory to workspace

---
+++ API/API/API.h
@@ -0,0 +1,19 @@
+//
+//  API.h
+//  API
+//
+//  Created by Jose Alvarez on 3/17/20.
+//  Copyright © 2020 DEUCE. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for API.
+FOUNDATION_EXPORT double APIVersionNumber;
+
+//! Project version string for API.
+FOUNDATION_EXPORT const unsigned char APIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <API/PublicHeader.h>
+
+
